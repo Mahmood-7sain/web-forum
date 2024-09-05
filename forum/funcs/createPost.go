f802035a9384e53bf81adb068c87ae0421810d91
malmarzo <m.h.almarzooq@gmail.com>
2024-09-05 11:10:15 +0300

    Empty form msg

--- forum/funcs/createPost.go
+++ forum/funcs/createPost.go
@@ -54,7 +54,8 @@ func CreateHandler(w http.ResponseWriter, r *http.Request) {
 			formattedDate := now.Format("02/01/06")
 
 			data.Date = formattedDate
-			err := tpl.ExecuteTemplate(w, "create-post.html", &data)
+
+			err := tpl.ExecuteTemplate(w, "create-post.html", nil)
 			if err != nil {
 				res := Error{}
 				res.Code = 500
@@ -72,84 +73,96 @@ func CreateHandler(w http.ResponseWriter, r *http.Request) {
 
 		if strings.ReplaceAll(title, " ", "") == "" || len(categories) == 0 || strings.ReplaceAll(content, " ", "") == "" {
 			// Redirect the user back to same page if he send empty form
-			http.Redirect(w, r, "/createPost", http.StatusSeeOther)
-			return
+			msg := ErrorMsg{}
+			msg.EmptyField = "Please fill all the fields!"
+
+			err := tpl.ExecuteTemplate(w, "create-post.html", &msg)
+
+			if err != nil {
+				res := Error{}
+				res.Code = 500
+				res.Status = "Internal server error"
+				ErrorHandler(w, r, &res)
+				return
+			}
+
+		} else {
+
+			id, found := GetSession(r)
+			if id == 0 && !found {
+				fmt.Println("No session found or id 0")
+				res := Error{}
+				res.Code = 400
+				res.Status = "Bad Request"
+				ErrorHandler(w, r, &res)
+				return
+			}
+
+			userExists, err := DB.CheckUser(id)
+			if err != nil {
+				fmt.Println("2")
+				res := Error{}
+				res.Code = 500
+				res.Status = "Internal server error"
+				ErrorHandler(w, r, &res)
+				return
+			}
+
+			if !userExists {
+				res := Error{}
+				res.Code = 400
+				res.Status = "Bad Request"
+				ErrorHandler(w, r, &res)
+				return
+			}
+
+			username, errGetUsername := DB.GetUsername(id)
+			if errGetUsername != nil {
+				res := Error{}
+				res.Code = 500
+				res.Status = "Internal server error"
+				ErrorHandler(w, r, &res)
+				return
+			}
+
+			if username == "" {
+				res := Error{}
+				res.Code = 400
+				res.Status = "Bad Request"
+				ErrorHandler(w, r, &res)
+				return
+			}
+
+			// Get the current date and time
+			now := time.Now()
+
+			// Format the date as dd/mm/yy
+			formattedDate := now.Format("02/01/06")
+
+			newPost := DB.Post{}
+			newPost.UserID = id
+			newPost.UserName = username
+			newPost.Title = title
+			newPost.Content = content
+			newPost.Date = formattedDate
+			newPost.Categories = categories
+			newPost.Likes = 0
+			newPost.Dislikes = 0
+			newPost.Comments = 0
+
+			success, errAdd := DB.AddPost(newPost)
+			if errAdd != nil || !success {
+				fmt.Println(errAdd)
+				res := Error{}
+				res.Code = 500
+				res.Status = "Internal server error"
+				ErrorHandler(w, r, &res)
+				return
+			}
+
+			// Redirect the user to the home page
+			http.Redirect(w, r, "/", http.StatusSeeOther)
 		}
 
-		id, found := GetSession(r)
-		if id == 0 && !found {
-			fmt.Println("No session found or id 0")
-			res := Error{}
-			res.Code = 400
-			res.Status = "Bad Request"
-			ErrorHandler(w, r, &res)
-			return
-		}
-
-		userExists, err := DB.CheckUser(id)
-		if err != nil {
-			fmt.Println("2")
-			res := Error{}
-			res.Code = 500
-			res.Status = "Internal server error"
-			ErrorHandler(w, r, &res)
-			return
-		}
-
-		if !userExists {
-			res := Error{}
-			res.Code = 400
-			res.Status = "Bad Request"
-			ErrorHandler(w, r, &res)
-			return
-		}
-
-		username, errGetUsername := DB.GetUsername(id)
-		if errGetUsername != nil {
-			res := Error{}
-			res.Code = 500
-			res.Status = "Internal server error"
-			ErrorHandler(w, r, &res)
-			return
-		}
-
-		if username == "" {
-			res := Error{}
-			res.Code = 400
-			res.Status = "Bad Request"
-			ErrorHandler(w, r, &res)
-			return
-		}
-
-
-		// Get the current date and time
-		now := time.Now()
-
-		// Format the date as dd/mm/yy
-		formattedDate := now.Format("02/01/06")
-
-		newPost := DB.Post{}
-		newPost.UserID = id
-		newPost.UserName = username
-		newPost.Title = title
-		newPost.Content = content
-		newPost.Date = formattedDate
-		newPost.Categories = categories
-		newPost.Likes = 0
-		newPost.Dislikes = 0
-		newPost.Comments = 0
-
-		success, errAdd := DB.AddPost(newPost)
-		if errAdd != nil || !success {
-			fmt.Println(errAdd)
-			res := Error{}
-			res.Code = 500
-			res.Status = "Internal server error"
-			ErrorHandler(w, r, &res)
-			return
-		}
-
-		// Redirect the user to the home page
-		http.Redirect(w, r, "/", http.StatusSeeOther)
 	}
 }
